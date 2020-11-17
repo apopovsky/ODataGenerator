b9ec83ece7e1084abc500a056344f8b525e72293
Ariel Popovsky <apopovsky@gmail.com>
2020-11-17 09:28:56 -0300

    Add project files.

---
+++ QueryGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ODataGenerator
+{
+    public class QueryGenerator<T>
+    {
+        public string Generate(Expression<Func<T,object>> expression)
+        {
+            string result = String.Empty;
+
+            return result;
+        }
+    }
+}
